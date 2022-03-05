d66ecbce450461ffe96283de6ad9dc7dfdca582d
produy <92656213+ntduydu2004@users.noreply.github.com>
2022-03-05 14:55:29 +0700

    Add files via upload

---
+++ time.h
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Date{
+    int dd, mm, yy;
+};
+struct Time{
+    int min, hour;
+};
---
+++ user.h
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "time.h"
+using namespace std;
+const int maxCourse = 5;
+struct student{
+    string ID;
+    string firstName;
+    string lastName;
+    string Class;
+    bool gender;
+    Date dob;
+    string SocialID;
+    // mark
+};
+struct staff{
+    string ID;
+    string firstName;
+    string lastName;
+    bool gender;
+    Date dob;
+    string SocialID;
+};
