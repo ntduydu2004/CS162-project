0b2921f2d807fbad3eaf7d5315307cf5fc238727
NgHuuTrong <trongnh2003@gmail.com>
2022-03-19 06:07:41 +0700

    Test gitignore

---
+++ Manager/Manager/CustomTime.h
@@ -0,0 +1,10 @@
+#pragma once
+//#include <bits/stdc++.h>
+using namespace std;
+
+struct Date {
+    int day, month, year;
+};
+struct Time {
+    int min, hour;
+};
---
+++ Manager/Manager/user.cpp
@@ -0,0 +1,35 @@
+//#include <bits/stdc++.h>
+#include "user.h"
+#include "Linkedlist.h"
+
+#include <stdio.h>
+#include <iostream>
+
+
+using namespace std;
+
+void dangnhap(user x)
+{
+    cout << "Ten dang nhap: ";
+    cin >> x.ID;
+    cout << "matkhau: ";
+    cin >> x.matkhau;
+    x.chucvu = -1;
+}
+void checkuser(user &x)// lưu danh sách users vô hàm main trước
+{
+    do
+    {
+        Node<user>* cur = pUserHead;
+        while (cur != NULL){
+            if (cur->data.ID == x.ID && cur->data.matkhau == x.matkhau){
+                x.chucvu == cur->data.chucvu;
+                return;
+            }
+            cur = cur->next;
+        }
+        cout << "Ten dang nhap hoac mat khau khong dung!";
+        dangnhap(x);
+    } while(1);
+    
+}
