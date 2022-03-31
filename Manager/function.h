aa60508d59b6f17ed4ae125dae82ea7a1cbac8cd
YangTuanAnh <yangtuananh2003@gmail.com>
2022-03-31 09:15:58 +0700

    Remove debug, duplicate files

--- Manager/function.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "user.h"
-void menuchinh();
-void studentFunc();
-void outputstudent(Student p);
-void inputstudent(Student& p);
-void staffFunc();
-void outputstaff(Staff p);
-void inputstaff(Staff& p);
